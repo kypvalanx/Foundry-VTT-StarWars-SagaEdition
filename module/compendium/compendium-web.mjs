835e67b10dd056ace62bbbe41228d364086afb32
kypvalanx <lijewskirocks@gmail.com>
2024-05-02 13:27:51 -0500

    * removed unused code and fixed default size

--- module/compendium/compendium-web.mjs
+++ module/compendium/compendium-web.mjs
@@ -1,143 +1,49 @@
 import {getIndexEntriesByTypes} from "./compendium-util.mjs";
 import {meetsPrerequisites} from "../prerequisite.mjs";
 
-const SPECIES = [{"value": "Abinyshi", "display": "Abinyshi"}, {
-    "value": "Farghul",
-    "display": "Farghul"
-}, {"value": "Ryn", "display": "Ryn"}, {"value": "Gungan", "display": "Gungan"}, {
-    "value": "Assembler",
-    "display": "Assembler"
-}, {"value": "Charon", "display": "Charon"}, {"value": "Weequay", "display": "Weequay"}, {
-    "value": "Gamorrean",
-    "display": "Gamorrean"
-}, {"value": "Terrelian", "display": "Terrelian"}, {"value": "Ranat", "display": "Ranat"}, {
-    "value": "Dashade",
-    "display": "Dashade"
-}, {"value": "Gossam", "display": "Gossam"}, {"value": "Amaran", "display": "Amaran"}, {
-    "value": "Codru-Ji",
-    "display": "Codru-Ji"
-}, {"value": "Defel", "display": "Defel"}, {"value": "Gurlanin", "display": "Gurlanin"}, {
-    "value": "Klatooinian",
-    "display": "Klatooinian"
-}, {"value": "Kudon", "display": "Kudon"}, {"value": "Nelvaanian", "display": "Nelvaanian"}, {
-    "value": "Ranth",
-    "display": "Ranth"
-}, {"value": "Shistavanen", "display": "Shistavanen"}, {"value": "Squib", "display": "Squib"}, {
-    "value": "Kissai",
-    "display": "Kissai"
-}, {"value": "Sith Offshoot", "display": "Sith Offshoot"}, {
-    "value": "Zuguruk",
-    "display": "Zuguruk"
-}, {"value": "Yuuzhan Vong", "display": "Yuuzhan Vong"}, {"value": "Bothan", "display": "Bothan"}, {
-    "value": "Kaleesh",
-    "display": "Kaleesh"
-}, {"value": "Kilmaulsi", "display": "Kilmaulsi"}, {
-    "value": "Rattataki",
-    "display": "Rattataki"
-}, {"value": "Trogodile", "display": "Trogodile"}, {"value": "Verpine", "display": "Verpine"}, {
-    "value": "Mon Calamari",
-    "display": "Mon Calamari"
-}, {"value": "Trandoshan", "display": "Trandoshan"}, {"value": "Cerean", "display": "Cerean"}, {
-    "value": "Kaminoan",
-    "display": "Kaminoan"
-}, {"value": "Bith", "display": "Bith"}, {"value": "Chadra-Fan", "display": "Chadra-Fan"}, {
-    "value": "Bimm",
-    "display": "Bimm"
-}, {"value": "Ho'Din", "display": "Ho'Din"}, {"value": "Gran", "display": "Gran"}, {
-    "value": "Trodatome",
-    "display": "Trodatome"
-}, {"value": "Rodisar", "display": "Rodisar"}, {"value": "Quarren", "display": "Quarren"}, {
-    "value": "Zabrak",
-    "display": "Zabrak"
-}, {"value": "Arcona", "display": "Arcona"}, {"value": "Veknoid", "display": "Veknoid"}, {
-    "value": "Yarkora",
-    "display": "Yarkora"
-}, {"value": "Utai", "display": "Utai"}, {"value": "Chagrian", "display": "Chagrian"}, {
-    "value": "Iktotchi",
-    "display": "Iktotchi"
-}, {"value": "Givin", "display": "Givin"}, {"value": "Kel Dor", "display": "Kel Dor"}, {
-    "value": "Elomin",
-    "display": "Elomin"
-}, {"value": "Chiss", "display": "Chiss"}, {"value": "Ebruchi", "display": "Ebruchi"}, {
-    "value": "Huralok",
-    "display": "Huralok"
-}, {"value": "Togorian", "display": "Togorian"}, {"value": "Zygerrian", "display": "Zygerrian"}, {
-    "value": "Siniteen",
-    "display": "Siniteen"
-}, {"value": "Cyclorrian", "display": "Cyclorrian"}, {
-    "value": "Imroosian",
-    "display": "Imroosian"
-}, {"value": "Togruta", "display": "Togruta"}, {"value": "Devaronian", "display": "Devaronian"}, {
-    "value": "Pa'lowick",
-    "display": "Pa'lowick"
-}, {"value": "Gand", "display": "Gand"}, {"value": "Herglic", "display": "Herglic"}, {
-    "value": "Blood Carver",
-    "display": "Blood Carver"
-}, {"value": "Noghri", "display": "Noghri"}, {"value": "Taung", "display": "Taung"}, {
-    "value": "Nikto",
-    "display": "Nikto"
-}, {"value": "Falleen", "display": "Falleen"}, {"value": "Hysalrian", "display": "Hysalrian"}, {
-    "value": "Thisspiasian",
-    "display": "Thisspiasian"
-}, {"value": "Nimbanel", "display": "Nimbanel"}, {"value": "Paigun", "display": "Paigun"}, {
-    "value": "Mirialan",
-    "display": "Mirialan"
-}, {"value": "Ithorian", "display": "Ithorian"}, {"value": "Ewok", "display": "Ewok"}, {
-    "value": "Duros",
-    "display": "Duros"
-}, {"value": "Neimoidian", "display": "Neimoidian"}, {"value": "Kubaz", "display": "Kubaz"}, {
-    "value": "Hutt",
-    "display": "Hutt"
-}, {"value": "Omwati", "display": "Omwati"}, {"value": "Twi'lek", "display": "Twi'lek"}, {
-    "value": "Tusken Raider",
-    "display": "Tusken Raider"
-}, {"value": "Snivvian", "display": "Snivvian"}, {"value": "Devlikk", "display": "Devlikk"}, {
-    "value": "Didynon",
-    "display": "Didynon"
-}, {"value": "Sephi", "display": "Sephi"}, {"value": "Firrerreo", "display": "Firrerreo"}, {
-    "value": "Rodian",
-    "display": "Rodian"
-}, {"value": "Sullustan", "display": "Sullustan"}, {"value": "Reesarian", "display": "Reesarian"}, {
-    "value": "Baragwin",
-    "display": "Baragwin"
-}, {"value": "Aqualish", "display": "Aqualish"}, {"value": "Ortolan", "display": "Ortolan"}, {
-    "value": "Cathar",
-    "display": "Cathar"
-}, {"value": "Wookiee", "display": "Wookiee"}, {"value": "Aki-Aki", "display": "Aki-Aki"}, {
-    "value": "Selkath",
-    "display": "Selkath"
-}, {"value": "Miraluka", "display": "Miraluka"}, {"value": "Yevetha", "display": "Yevetha"}, {
-    "value": "Catuman",
-    "display": "Catuman"
-}, {"value": "Hrakian", "display": "Hrakian"}, {"value": "Human", "display": "Human"}, {
-    "value": "Octeroid",
-    "display": "Octeroid"
-}, {"value": "Zeltron", "display": "Zeltron"}, {"value": "Trianii", "display": "Trianii"}, {
-    "value": "Kessurian",
-    "display": "Kessurian"
-}, {"value": "Tridactyl", "display": "Tridactyl"}, {
-    "value": "Dybrinthe",
-    "display": "Dybrinthe"
-}, {"value": "Geonosian", "display": "Geonosian"}, {"value": "Lannik", "display": "Lannik"}, {
-    "value": "Yuzzum",
-    "display": "Yuzzum"
-}, {"value": "Draethos", "display": "Draethos"}, {"value": "Jawa", "display": "Jawa"}, {
-    "value": "Teedo",
-    "display": "Teedo"
-}, {"value": "Massassi", "display": "Massassi"}, {"value": "Murachaun", "display": "Murachaun"}, {
-    "value": "Ubese",
-    "display": "Ubese"
-}, {"value": "Polis Massan", "display": "Polis Massan"}, {
-    "value": "Chevin",
-    "display": "Chevin"
-}, {"value": "T'landa Til", "display": "T'landa Til"}, {"value": "Medical Droid", "display": "Medical Droid"}]
-
-const SKIP_LINK = ["Weapon Focus", "Dual Weapon Mastery I", "Double Attack", "Martial Arts I", "Dodge", "Weapon Proficiency", "Skill Focus", "Point-Blank Shot"];
-
 export class CompendiumWeb extends Application {
 
     static _pattern = /\s\([\w#\s]*\)/
     static _payloadPattern = new RegExp(CompendiumWeb._pattern, "g");
+
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            template: "systems/swse/templates/compendium/compendium-web.hbs",
+            id: null,
+            popOut: true,
+            width: 700,
+            height: 1400,
+            classes: ["web", "swse"],
+            resizable: true,
+            baseApplication: "CompendiumWeb"
+        });
+    }
+
+    constructor(...args) {
+        super(...args);
+
+        Hooks.on("renderApplication", async (event, target) => {
+            if (event.appId !== this.appId) { //is this really the best way to do this?  weird that applications aren't more contained
+                return;
+            }
+
+            this.options = args[0];
+            this.target = target;
+
+            this.types = ['feat', 'talent']
+
+            if (this.options?.types) {
+                this.types = this.options.types;
+            }
+
+            await this.addFilters(target, this.types)
+            await this.populateFiltersFromArguments(target, this.options);
+            await this.renderWeb(event, target, this.types);
+
+            //currently disabled.  will allow arrows to be drawn in the future
+            //await this.drawArrows(target)
+        })
+    }
 
     /**
      *
@@ -181,7 +87,9 @@ export class CompendiumWeb extends Application {
             },
             options: async (types) => {
 
-                return [...await getIndexEntriesByTypes(types)].flatMap(([key, item]) => CompendiumWeb.getPrerequisitesByType(item.system.prerequisite, ["SPECIES"])).map(p => p.requirement).distinct().map(s => {return {display:s, value:s}})
+                return [...await getIndexEntriesByTypes(types)].flatMap(([key, item]) => CompendiumWeb.getPrerequisitesByType(item.system.prerequisite, ["SPECIES"])).map(p => p.requirement).distinct().map(s => {
+                    return {display: s, value: s}
+                })
 
                 //return SPECIES;
             },
@@ -192,14 +100,14 @@ export class CompendiumWeb extends Application {
             multiple: false,
             selector: "actor-filter",
             name: "Filter out by actor stats",
-            mutation:  (value) => {
+            mutation: (value) => {
                 return (index) => {
                     const actor = game.actors.get(value)
                     index.hide ||= !(meetsPrerequisites(actor, index.prerequisite)).doesFail;
                     //index.actorHas
                 }
             },
-            options:  () => {
+            options: () => {
                 return game.actors.filter(a => a.canUserModify(game.user, 'update')).map(a => {
                     return {value: a.id, display: a.name}
                 });
@@ -228,7 +136,10 @@ export class CompendiumWeb extends Application {
             },
             options: async (types) => {
                 return [...await getIndexEntriesByTypes(types)].map(([key, item]) => item.system.source).distinct().filter(i => !!i).map(book => {
-                    return {value: book, display: book.replace("Star Wars Saga Edition", "SWSE").replace("Clone Wars Saga Edition", "CWSE")}
+                    return {
+                        value: book,
+                        display: book.replace("Star Wars Saga Edition", "SWSE").replace("Clone Wars Saga Edition", "CWSE")
+                    }
                 });
             },
             allowExclude: true
@@ -240,7 +151,7 @@ export class CompendiumWeb extends Application {
             name: "Filter out Homebrew",
             mutation: (value) => {
                 return (index) => {
-                    if(value){
+                    if (value) {
                         index.hide ||= index.homebrew;
                     }
                 }
@@ -259,39 +170,8 @@ export class CompendiumWeb extends Application {
         }
     ]
 
-    constructor(...args) {
-        super(...args);
-
-        Hooks.on("renderApplication", async (event, target) => {
-            if(event.appId !== this.appId){ //is this really the best way to do this?  weird that applications aren't more contained
-                return;
-            }
-
-            this.options = args[0];
-            this.target = target;
-
-            this.types = ['feat', 'talent']
-
-            if (this.options?.types) {
-                this.types = this.options.types;
-            }
-
-            await this.addFilters(target,this.types)
-            await this.populateFiltersFromArguments(target, this.options);
-            await this.renderWeb(event, target, this.types);
-
-            //currently disabled.  will allow arrows to be drawn in the future
-            //await this.drawArrows(target)
-        })
-    }
-
     async renderWeb(e, target, types) {
-
-
         let items = await getIndexEntriesByTypes(types)
-
-        //console.log(items)
-        //const skipLinking = this.getSkippedUuids(items);
 
         const dependencyMap = new Map()
         const invertedDependencyMap = new Map();
@@ -344,9 +224,6 @@ export class CompendiumWeb extends Application {
             .filter(([key, values]) => values && values.filter(value => this.shouldDraw(value, itemFilterMeta, invertedDependencyMap)).length > 10)
             .map(([key, values]) => key)
 
-
-        //console.log(skipLinking, skipLinking.length, skipLinking2, skipLinking2.length)
-
         const rootUuids = rootItems.map(r => r.uuid);
         const groupedIds = []
         const groups = []
@@ -357,7 +234,6 @@ export class CompendiumWeb extends Application {
             }
 
             const groupNodes = [rootUuid]
-            //const rootNodes = [rootUuid]
 
             let children = invertedDependencyMap.get(rootUuid)
             if (!children) {
@@ -384,17 +260,11 @@ export class CompendiumWeb extends Application {
                     if (invertedDependencyMap.has(activeNode) && shouldLink) {
                         newNodes.push(...invertedDependencyMap.get(activeNode))
                     }
-                    // if (rootUuids.includes(activeNode)) {
-                    //     rootNodes.push(activeNode)
-                    // }
                 }
             }
 
             groups.push({groupNodes: groupNodes.distinct()})
         }
-
-
-        //console.log(groups)
 
         const depths = new Map()
         let deepestBranch = 0;
@@ -428,7 +298,7 @@ export class CompendiumWeb extends Application {
             homebrew: item.isHomeBrew,
             baseAttackBonus: bab,
             species: speciesPrerequisites,
-            talentTree:item.system.talentTree,
+            talentTree: item.system.talentTree,
             prerequisite: item.system.prerequisite
         };
     }
@@ -440,11 +310,6 @@ export class CompendiumWeb extends Application {
         }
         return Math.max(...children.map(child => this.getDepth(child, dependencyMap))) + 1
     }
-
-    getSkippedUuids(items) {
-        return [...items].filter(([key, item]) => SKIP_LINK.includes(item.name)).map(([key, item]) => item.uuid) || []
-    }
-
 
     /**
      *
@@ -472,8 +337,8 @@ export class CompendiumWeb extends Application {
         babs.push(0)
         return Math.max(...babs);
     }
-    async createGroup(grouping, deepestLevel, levels, items, groupNumber, invertedDependencyMapping, metaMapping) {
 
+    async createGroup(grouping, deepestLevel, levels, items, groupNumber, invertedDependencyMapping, metaMapping) {
         const itemsByLevel = [];
         for (const groupingElement of grouping) {
             const level = levels.get(groupingElement);
@@ -536,7 +401,7 @@ export class CompendiumWeb extends Application {
 
         const itemArea = $(`<div class="web-item"></div>`);
         itemArea.append(img)
-        if(item.type === "talent"){
+        if (item.type === "talent") {
             itemArea.append($(`<div class="text talent">${item.system.talentTree}:</div>`))
         }
         itemArea.append($(`<div class="text">${item.name}</div>`))
@@ -568,19 +433,6 @@ export class CompendiumWeb extends Application {
         return prerequisites;
     }
 
-    static get defaultOptions() {
-        return foundry.utils.mergeObject(super.defaultOptions, {
-            template: "systems/swse/templates/compendium/compendium-web.hbs",
-            id: null,
-            popOut: true,
-            width: 300,
-            height: "auto",
-            classes: ["web", "swse"],
-            resizable: true,
-            baseApplication: "CompendiumWeb"
-        });
-    }
-
     get id() {
         return `${this.options.id}${this._original ? "-popout" : ""}`;
     }
@@ -592,32 +444,6 @@ export class CompendiumWeb extends Application {
             user: game.user
         };
     }
-
-
-    getWebLevel(dependencyMap, name, level) {
-        let dependencies = dependencyMap[name];
-        if (!dependencies) {
-            let payloadFree = name.replace(CompendiumWeb._payloadPattern, "");
-            dependencies = dependencyMap[payloadFree]
-        }
-
-        if (!dependencies) {
-            return {level, lowestNodes: [name], allNodes: [name]};
-        }
-
-        let resultingLevel = 0
-        let nodes = [];
-        let allNodes = [name];
-        for (const dependency of dependencies) {
-            const webLevel = this.getWebLevel(dependencyMap, dependency.requirement, level + 1)
-            resultingLevel = Math.max(webLevel.level, resultingLevel);
-            nodes.push(...webLevel.lowestNodes);
-            allNodes.push(...webLevel.allNodes);
-        }
-
-        return {level: resultingLevel, lowestNodes: nodes.distinct(), allNodes: allNodes.distinct()}
-    }
-
     async populateFiltersFromArguments(target, options) {
 
         if (options.webFilters) {
@@ -684,7 +510,7 @@ export class CompendiumWeb extends Application {
             default:
                 filterComponent = $(`<div>unsupported filter type</div>`)
         }
-       const containerId = `${filter.selector}`
+        const containerId = `${filter.selector}`
 
         filterComponent.addClass(filter.selector)
         filterComponent.addClass("value")
@@ -707,6 +533,7 @@ export class CompendiumWeb extends Application {
 
         return topContainer;
     }
+
     async drawArrows(target) {
         const found = target.find(".mappable")
         const webviewer = target.find(".web-viewer")
@@ -724,38 +551,5 @@ export class CompendiumWeb extends Application {
             }
         }
         //webviewer.append(arrows)
-    }
-
-    getOffset(el) {
-        var rect = el.getBoundingClientRect();
-        return {
-            left: rect.left + window.pageXOffset,
-            top: rect.top + window.pageYOffset,
-            width: rect.width || el.offsetWidth,
-            height: rect.height || el.offsetHeight
-        };
-    }
-
-    connect(div1, div2, color, thickness) { // draw a line connecting elements
-        const off1 = this.getOffset(div1);
-        var off2 = this.getOffset(div2);
-        // bottom right
-        var x1 = off1.left + off1.width;
-        var y1 = off1.top + off1.height;
-        // top right
-        var x2 = off2.left + off2.width;
-        var y2 = off2.top;
-        // distance
-        var length = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
-        // center
-        var cx = ((x1 + x2) / 2) - (length / 2);
-        var cy = ((y1 + y2) / 2) - (thickness / 2);
-        // angle
-        var angle = Math.atan2((y1 - y2), (x1 - x2)) * (180 / Math.PI);
-        // make hr
-        var htmlLine = "<div style='padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
-        //
-        // alert(htmlLine);
-        document.body.innerHTML += htmlLine;
     }
 }
