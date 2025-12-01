36b5c930345433e8e0e770dcd2bfc6efe4370691
kypvalanx <lijewskirocks@gmail.com>
2025-12-01 13:44:01 -0600

    Removed redundant actor lookup in `game.data.actors` for streamlined ID resolution.

--- module/common/util.mjs
+++ module/common/util.mjs
@@ -1876,7 +1876,6 @@ export const getActorFromId = function (id) {
     if (id) {
         actor = game.actors?.tokens[id]
         if (!actor) actor = game.actors?.get(id);
-        if (!actor) actor = game.data.actors.find(a => a._id === id);
     }
     if (!actor) {
         const speaker = ChatMessage.getSpeaker();
