bd0af207aadfa84cb440f28ec3909ac0213c3e4e
kypvalanx <lijewskirocks@gmail.com>
2026-02-01 05:28:22 -0600

    updated `appendTerms` tests to include new `modifiers` and `prerequisites` properties in term JSON validation

--- module_test/quench/actor/attack/attack.test.mjs
+++ module_test/quench/actor/attack/attack.test.mjs
@@ -7,30 +7,20 @@ export async function attackTests(quench) {
             const {describe, it, assert, expect, should} = context;
             describe("appendTerm", () => {
                 it("should append the term", async () => {
-                    assert.equal(`[{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"+"}` +
-                        `,{"class":"NumericTerm","options":{"flavor":"bonus"},"evaluated":false,"number":1}]`, JSON.stringify(appendTerms(1, "bonus")))
+                    assert.equal(`[{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"+"},{"class":"NumericTerm","options":{"flavor":"bonus","modifiers":[],"prerequisites":[]},"evaluated":false,"number":1}]`, JSON.stringify(appendTerms(1, "bonus")))
                 })
                 it("should append the term", async () => {
-                    assert.equal(`[{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"-"}` +
-                        `,{"class":"NumericTerm","options":{"flavor":"bonus"},"evaluated":false,"number":2}]`, JSON.stringify(appendTerms("-2", "bonus")))
+                    assert.equal(`[{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"-"},{"class":"NumericTerm","options":{"flavor":"bonus","modifiers":[],"prerequisites":[]},"evaluated":false,"number":2}]`, JSON.stringify(appendTerms("-2", "bonus")))
                 })
                 it("should append the term", async () => {
                     assert.equal(`[{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"-"}` +
                         `,{"class":"Die","options":{"flavor":"bonus"},"evaluated":false,"number":4,"faces":10,"modifiers":[],"results":[]}]`, JSON.stringify(appendTerms("-4d10", "bonus")))
                 })
                 it("should append the term", async () => {
-                    assert.equal(`[{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"-"}` +
-                        `,{"class":"Die","options":{"flavor":"bonus"},"evaluated":false,"number":4,"faces":10,"modifiers":[],"results":[]}` +
-                        `,{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"-"},` +
-                        `{"class":"Die","options":{"flavor":"bonus"},"evaluated":false,"number":6,"faces":4,"modifiers":[],"results":[]},` +
-                        `{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"+"},` +
-                        `{"class":"NumericTerm","options":{"flavor":"bonus"},"evaluated":false,"number":9}]`, JSON.stringify(appendTerms("-4d10-6d4+9", "bonus")))
+                    assert.equal(`[{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"-"},{"class":"Die","options":{"flavor":"bonus"},"evaluated":false,"number":4,"faces":10,"modifiers":[],"results":[]},{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"-"},{"class":"Die","options":{"flavor":"bonus"},"evaluated":false,"number":6,"faces":4,"modifiers":[],"results":[]},{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"+"},{"class":"NumericTerm","options":{"flavor":"bonus","modifiers":[],"prerequisites":[]},"evaluated":false,"number":9}]`, JSON.stringify(appendTerms("-4d10-6d4+9", "bonus")))
                 })
                 it("should append the term", async () => {
-                    assert.equal(`[{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"+"}` +
-                        `,{"class":"NumericTerm","options":{"flavor":"bomb"},"evaluated":false,"number":1}` +
-                        `,{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"+"}` +
-                        `,{"class":"Die","options":{"flavor":"bomb"},"evaluated":false,"number":3,"faces":6,"modifiers":[],"results":[]}]`, JSON.stringify(appendTerms("1+3d6", "bomb")))
+                    assert.equal(`[{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"+"},{"class":"NumericTerm","options":{"flavor":"bomb","modifiers":[],"prerequisites":[]},"evaluated":false,"number":1},{"class":"OperatorTerm","options":{},"evaluated":false,"operator":"+"},{"class":"Die","options":{"flavor":"bomb"},"evaluated":false,"number":3,"faces":6,"modifiers":[],"results":[]}]`, JSON.stringify(appendTerms("1+3d6", "bomb")))
                 })
             })
         })
