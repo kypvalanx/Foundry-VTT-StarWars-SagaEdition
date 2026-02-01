45aacb3808d4deb402acb07d0962189ce10bf2c0
kypvalanx <lijewskirocks@gmail.com>
2026-02-01 05:30:32 -0600

    fixed assertion calls in martial artist feats test by removing incorrect `roll` property access

--- module_test/quench/actor/actor-sheet.feats-martial-artist.test.mjs
+++ module_test/quench/actor/actor-sheet.feats-martial-artist.test.mjs
@@ -19,13 +19,13 @@ export async function featTests(quench) {
                             await actor.sheet._onDropItem(getMockEvent(), {name: "Soldier", type: "class"})
                             await actor.sheet._onDropItem(getMockEvent(), {name: "Soldier", type: "class"})
                             await actor.sheet._onDropItem(getMockEvent(), {name: "Soldier", type: "class"})
-                            assert.equal(actor.attack.attacks[0].damageRoll.roll.dice[0].formula, "1d4", "UAE for medium creatures should be 1d4")
+                            assert.equal(actor.attack.attacks[0].damageRoll.dice[0].formula, "1d4", "UAE for medium creatures should be 1d4")
                             await actor.sheet._onDropItem(getMockEvent(), {name: "Martial Arts I", type: "feat", answers: ["Soldier Bonus Feats"]})
-                            assert.equal(actor.attack.attacks[0].damageRoll.roll.dice[0].formula, "1d6", "UAE for medium creatures with Martial Arts I should be 1d6")
+                            assert.equal(actor.attack.attacks[0].damageRoll.dice[0].formula, "1d6", "UAE for medium creatures with Martial Arts I should be 1d6")
                             await actor.sheet._onDropItem(getMockEvent(), {name: "Martial Arts II", type: "feat", answers: ["Soldier Bonus Feats"]})
-                            assert.equal(actor.attack.attacks[0].damageRoll.roll.dice[0].formula, "1d8", "UAE for medium creatures with Martial Arts II should be 1d8")
+                            assert.equal(actor.attack.attacks[0].damageRoll.dice[0].formula, "1d8", "UAE for medium creatures with Martial Arts II should be 1d8")
                             await actor.sheet._onDropItem(getMockEvent(), {name: "Martial Arts III", type: "feat", answers: ["Soldier Bonus Feats"]})
-                            assert.equal(actor.attack.attacks[0].damageRoll.roll.dice[0].formula, "1d10", "UAE for medium creatures with Martial Arts III should be 1d10")
+                            assert.equal(actor.attack.attacks[0].damageRoll.dice[0].formula, "1d10", "UAE for medium creatures with Martial Arts III should be 1d10")
 
 
                         })
